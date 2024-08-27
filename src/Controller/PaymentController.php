f170f506bf1105d5e9a8d9c61eaa12bb85acc6b4
Vi Long LUONG <vlluong@datasolution.fr>
2024-08-27 14:46:45 +0200

    Payment with Paypal

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -6,11 +6,17 @@ use App\Entity\Order;
 use App\Entity\Product;
 use App\Service\CartService;
 use Doctrine\ORM\EntityManagerInterface;
+use PayPalCheckoutSdk\Core\PayPalHttpClient;
+use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
+use PayPalHttp\HttpException;
+use PayPalHttp\IOException;
 use Stripe\Checkout\Session;
 use Stripe\Exception\ApiErrorException;
 use Stripe\Stripe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -26,7 +32,7 @@ class PaymentController extends AbstractController
     /**
      * @throws ApiErrorException
      */
-    #[Route('/order/create-session-stripe/{reference}', name: 'payment_stripe')]
+    #[Route('/order/create-session-stripe/{reference}', name: 'payment_stripe', methods: ['POST'])]
     public function stripeCheckout($reference) : RedirectResponse
     {
         $order = $this->entityManager->getRepository(Order::class)->findOneBy(['reference' => $reference]);
@@ -84,15 +90,134 @@ class PaymentController extends AbstractController
         return new RedirectResponse($checkout_session->url);
     }
 
+    public function getPaypalClient(): PayPalHttpClient
+    {
+        $clientId = "AYxIuXN8fC2eAzm9ZZL6KTAx69xb-K6wu2pqAqdqEYDtukyWM5qwmziIFppcpDWQB-Je74JPIRHtES4A";
+        $clientSecret = "ELgdYRRL9jvwEWuxg5W-zBM0hFkeABMQ2oNBMY48qBk8pfV1SWBynvs4pmEn8bpgNB-MrmWbfZ1THcwx";
+        $environment = new SandboxEnvironment($clientId, $clientSecret);
+        return new PayPalHttpClient($environment);
+    }
+
+    /**
+     * @throws HttpException
+     * @throws IOException
+     */
+    #[Route('/order/create-session-paypal/{reference}', name: 'payment_paypal', methods: ['POST'])]
+    public function paypalCheckout($reference) : RedirectResponse
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneBy(['reference' => $reference]);
+        if (!$order) {
+            return $this->redirectToRoute('cart_index');
+        }
+
+        $items = [];
+        $itemTotal = 0;
+
+        foreach ($order->getRecapDetails()->getValues() as $item)
+        {
+            $items[] = [
+                'name' => $item->getProduct(),
+                'quantity' => $item->getQuantity(),
+                'unit_amount' => [
+                    'value' => $item->getPrice(),
+                    'currency_code' => 'EUR',
+                ],
+            ];
+            $itemTotal += $item->getPrice() * $item->getQuantity();
+        }
+
+        $total = $itemTotal + $order->getTransporterPrice();
+
+        $request = new OrdersCreateRequest();
+        $request->prefer('return=representation');
+        $request->body = [
+            'intent' => 'CAPTURE',
+            'purchase_units' => [
+                [
+                    'amount' => [
+                        'currency_code' => 'EUR',
+                        'value' => $total,
+                        'breakdown' => [
+                            'item_total' => [
+                                'currency_code' => 'EUR',
+                                'value' => $itemTotal,
+                            ],
+                            'shipping' => [
+                                'currency_code' => 'EUR',
+                                'value' => $order->getTransporterPrice(),
+                            ]
+                        ]
+                    ],
+                    'items' => $items
+                ]
+            ],
+            'application_context' => [
+                'return_url' => $this->urlGenerator->generate('payment_success', [
+                    'reference' => $order->getReference()],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                ),
+                'cancel_url' => $this->urlGenerator->generate('payment_error', [
+                    'reference' => $order->getReference()],
+                    UrlGeneratorInterface::ABSOLUTE_URL
+                )
+            ]
+        ];
+
+        $client = $this->getPaypalClient();
+        $response = $client->execute($request);
+
+        if ($response->statusCode != 201) {
+            return $this->redirectToRoute('cart_index');
+        }
+
+        $approvalLink = '';
+        foreach ($response->result->links as $link) {
+            if ($link->rel === 'approve') {
+                $approvalLink = $link->href;
+                break;
+            }
+        }
+
+        if (empty($approvalLink)) {
+            return $this->redirectToRoute('cart_index');
+        }
+
+        $order->setPaypalOrderId($response->result->id);
+
+        $this->entityManager->flush();
+
+        return new RedirectResponse($approvalLink);
+    }
+
     #[Route('/order/success/{reference}', name: 'payment_success')]
     public function paymentSuccess($reference, CartService $cartService) : Response
     {
-        return $this->render('order/success.html.twig');
+        $order = $this->entityManager->getRepository(Order::class)->findOneBy(['reference' => $reference]);
+        if (!$order || $order->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('shop_index');
+        }
+
+        if (!$order->isPaid()) {
+            $cartService->removeCartAll();
+            $order->setPaid(true);
+            $this->entityManager->flush();
+        }
+
+        return $this->render('order/success.html.twig', [
+            'order' => $order
+        ]);
     }
 
     #[Route('/order/error/{reference}', name: 'payment_error')]
     public function paymentError($reference, CartService $cartService) : Response
     {
-        return $this->render('order/error.html.twig');
+        $order = $this->entityManager->getRepository(Order::class)->findOneBy(['reference' => $reference]);
+        if (!$order || $order->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('shop_index');
+        }
+
+        return $this->render('order/error.html.twig', [
+            'order' => $order
+        ]);
     }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -6,6 +6,7 @@ use App\Entity\Address;
 use App\Entity\Transporter;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -25,6 +26,16 @@ class OrderType extends AbstractType
             ])
             ->add('transporter', EntityType::class, [
                 'class' => Transporter::class,
+                'label' => false,
+                'required' => true,
+                'multiple' => false,
+                'expanded' => true,
+            ])
+            ->add('payment', ChoiceType::class, [
+                'choices' => [
+                    'Pay with Paypal' => 'paypal',
+                    'Pay with Stripe' => 'stripe',
+                ],
                 'label' => false,
                 'required' => true,
                 'multiple' => false,
